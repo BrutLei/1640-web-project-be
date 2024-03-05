d103ab902661bbccd78bdb8aa7a1d7fbb3c4031a
BrutLei <hoangphat4510@gmail.com>
2024-03-05 20:04:28 +0700

    .

--- server.js
+++ server.js
@@ -3,11 +3,27 @@ require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 8080;
-const hostName = process.env.HOST_NAME;
 
-// Khai báo route
-app.get('/', (req, res) => {
-  res.send('Hello nodemon');
+app.use(express.json());
+
+app.get('/shirt', (req, res) => {
+  res.status(200).send({
+    type: 't-shirt',
+    size: 'XXL',
+  });
+});
+
+app.post('/shirt/:id', (req, res) => {
+  const { id } = req.params;
+  const { logo } = req.body;
+
+  if (!logo) {
+    res.status(404).send({ message: 'we need a logo' });
+  }
+
+  res.status(200).send({
+    tshirt: `a shirt with your ${logo} on it`,
+  });
 });
 
 app.listen(port, () => {
